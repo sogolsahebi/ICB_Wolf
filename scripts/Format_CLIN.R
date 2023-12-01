b3026356dfa75ad47837e81ec8f40caecde88677
Nasim Sahebi <sogol.sahebi@mail.utoronto.ca>
2023-12-01 17:42:51 -0500

    update clinical data curated file

--- scripts/Format_CLIN.R
+++ scripts/Format_CLIN.R
@@ -1,5 +1,7 @@
 # Libraries and Source Files
 library(tibble)
+
+# Sourcing functions from GitHub for processing clinical data
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/Get_Response.R")
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/format_clin_data.R")
 source("https://raw.githubusercontent.com/BHKLAB-Pachyderm/ICB_Common/main/code/annotate_tissue.R")
@@ -11,68 +13,58 @@ clin <- read.csv(file_path_clin, stringsAsFactors = FALSE, sep = "\t")
 rownames(clin) <- clin$Patient.Identifier
 
 # Data Curation for Clinical Data
+# Selecting Specific Columns for Analysis
 selected_cols <- c("Patient.Identifier", "Arm", "pCR")
-clin <- cbind(clin[, selected_cols], "Lung", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
-colnames(clin) <- c("patient", "drug_type", "recist", "primary", "age", "sex", "stage", "response", "t.os", "t.pfs", "histo", "stage", "os", "pfs", "dna", "rna", "response.other.info")
+clin <- cbind(clin[, selected_cols], rep(NA, 15))
 
+colnames(clin) <- c("patient", "drug_type", "recist", "primary", "age", "sex", "stage", "response.other.info", "t.os", "t.pfs", "histo", "stage", "os", "pfs", "dna", "rna", "response")
 
+# Define response based on values in "response.other.info"
+clin$response <- Get_Response(data = clin) 
 
-# Error Handling and Response Calculation
-# clin$response = Get_Response(data = clin)  # Uncomment when Get_Response function is working
-
-
-# Modify stage values for better clarity.
+# Set sex, stage, dna, dna_info columns at NA
+clin$sex <- "F"
 clin$stage <- NA
-
-# Assign 'tpm' and 'wes' values based on RNA_All and WES_All respectively.
 clin$dna <- NA
-#clin$rna <- #TODO: after Farnoosh Mondays' comment 
+clin$dna_info <- NA
 
-#TODO: make sure we dont have two column of aems
+# Microarray and RNA-seq
+clin$rna <- "Microarray"
 
+# TODO: Check if linearization could be in rna_info
+clin$rna_info <- "NA"          
 
-# Reorder columns.
+# Reordering Columns for Structured Data
 clin <- clin[, c(
   "patient", "sex", "age", "primary", "histo", "stage", 
-  "response.other.info", "recist", "response", "drug_type", "dna", "rna", "t.pfs", 
+  "response.other.info", "recist", "response", "drug_type", "dna", "dna_info", "rna","rna_info", "t.pfs", 
   "pfs", "t.os", "os"
 )]
 
-
-# Use the format_clin_data function for further formatting.
+# Formatting clinical data using a custom function.
 clin <- format_clin_data(clin, "patient", selected_cols, clin)
 
-#add survival_unit and survival_type columns. and use  'curation_tissue.csv' file to set clin$tissueid column
-#annotation_tissue <- read.csv("~/BHK lab/Ravi/deleted repo/Ravi_version2/Common files/curation_tissue.csv")
+# Add survival_unit and survival_type columns, use 'curation_tissue.csv' file to set clin$tissueid column
+# TODO: Add Wolf to the data files
 
-#Read curation_tissue.cv file
-#TODO : add WOlf to the dafa files
+# Annotate Tissue Data
 path <- "https://raw.githubusercontent.com/BHKLAB-DataProcessing/ICB_Common/main/data/curation_tissue.csv"
-#annotation_tissue<- read.csv(file.path(annot_dir, 'curation_tissue.csv'))
-annotation_tissue<- read.csv(path )
+annotation_tissue <- read.csv(path)
 
 clin <- annotate_tissue(clin=clin, study='Wolf', annotation_tissue=annotation_tissue, check_histo=FALSE)
 
-#add column treatment id after tissueid column.
-clin <- add_column(clin, treatmentid= clin$Agent_PD1, .after='tissueid')
+# Add column 'treatmentid' after 'tissueid' column.
+clin <- add_column(clin, treatmentid = clin$Agent_PD1, .after='tissueid')
 
 # Adding drug_type based on treatmentid.
 # Print unique values of treatmentid.
 print(unique(clin$treatmentid))
 
-#clin$drug_typ<- # TODO: based on Farnoosh chategorization decision on adding chemo
-
-# Print unique values of drugtype
-print(unique(clin$drug_type))
+# clin$drug_typ <- # TODO: Based on Farnoosh categorization decision on adding chemo
 
 # Replace empty string values with NA.
 clin[clin == ""] <- NA
 
 # Save the processed data as CLIN.csv file
-#write.csv(clin , file=file.path(output_dir, "CLIN.csv") , row.names=TRUE )
-
 file <- "~/BHK lab/ICB_Wolf/data/CLIN.csv"
 write.csv(clin, file, row.names = TRUE)
-
-
-
